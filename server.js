4559e1d78e9ae36d57b52dbf092142615a223a63
Rudra032 <rudrarathore2842@gmail.com>
2024-03-25 00:30:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: 'dotenv' })
+import express from "express";
+import { db } from "./db.js";
+import roomsRoute from "./routes/roomsRouter.js";
+import userRouter from "./routes/userRouter.js";
+import bookingsRouter from "./routes/bookingsRouter.js";
+// import { config as dotenvConfig } from 'dotenv';
+
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 5000;
+// console.log(process.env.PORT);
+
+
+app.use("/api/mern_rooms", roomsRoute);
+app.use("/api/users", userRouter);
+app.use("/api/bookings", bookingsRouter);
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
